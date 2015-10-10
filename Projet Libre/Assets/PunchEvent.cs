c08201f742202005358b8021b6b10b3356488755
Maestro-Gibli <gautier.melot@gmail.com>
2015-10-10 15:00:20 +0200

    Up

---
+++ Projet Libre/Assets/PunchEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchEvent : MonoBehaviour
+{
+	private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+			anim.SetTrigger("punch");	
+	}
+}
